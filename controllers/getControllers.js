e77126fc88bcfea45b7ec260f4162b2cccfda21a
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-07-09 22:05:16 +0600

    Added the get controllers

---
+++ controllers/getControllers.js
@@ -0,0 +1,22 @@
+import Product from "../models/product.model.js";
+
+export const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        });
+    }
+}
+
+export const getProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch(error) {
+        res.status(404).json({ message: error.message });
+    }
+}
--- routes/getProductsRoute.js
+++ routes/getProductsRoute.js
@@ -1,25 +1,10 @@
 import express from 'express';
-import Product from '../models/product.model.js';
+import { getProduct, getProducts } from '../controllers/getControllers.js'
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch(error) {
-        res.status(404).json({ message: error.message });
-    }
-});
+router.get('/', getProducts);
 
-router.get('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch(error) {
-        res.status(404).json({ message: error.message });
-    }
-});
+router.get('/:id', getProduct);
 
 export default router;
